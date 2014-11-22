fd8f86528fcaeb6b86111d3c1c20f3ab727439fb
Vincent <guofangsky@gmail.com>
2014-11-23 06:43:02 +0800

    performance tuning on : View Tag to cache object

--- ListViewTest/src/com/example/listviewtest/FruitAdapter.java
+++ ListViewTest/src/com/example/listviewtest/FruitAdapter.java
@@ -27,17 +27,34 @@ public class FruitAdapter extends ArrayAdapter<Fruit>{
 		// TODO Auto-generated method stub
 		Fruit fruit = this.getItem(position);
 		View view ;
+		// a inner class, to not get Widget every time 
+		ViewHolder viewHolder;
 		// it makes : even you scroll the screen very fast, it can perform good.  
 		if(convertView==null){
 			view = LayoutInflater.from(this.getContext()).inflate(resourceId, null);
+			viewHolder = new ViewHolder();
+			viewHolder.fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
+			viewHolder.fruitName=(TextView)view.findViewById(R.id.fruit_name);
+			//TODO ? save ViewHolder in View; what the detail usage 
+			view.setTag(viewHolder);
 		} else {
 			view = convertView;
+			viewHolder=(ViewHolder)view.getTag();// retrieve the saved ViewHolder
 		}
-		ImageView fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
-		TextView fruitName=(TextView)view.findViewById(R.id.fruit_name);
-		fruitImage.setImageResource(fruit.getImageId());
-		fruitName.setText(fruit.getName());
+//		ImageView fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
+//		TextView fruitName=(TextView)view.findViewById(R.id.fruit_name);
+//		fruitImage.setImageResource(fruit.getImageId());
+//		fruitName.setText(fruit.getName());
+		
+		viewHolder.fruitImage.setImageResource(fruit.getImageId());
+		viewHolder.fruitName.setText(fruit.getName());
+		
 		return view;
+	}
+	
+	class ViewHolder {
+		ImageView fruitImage;
+		TextView fruitName;
 	}
 	
 	
