8660c2872a0f1067a942a032cba9d3a9e6baa8e5
icersummer <guofangsky@gmail.com>
2014-11-27 17:42:57 +0800

    add own Project JokeKing

---
+++ JokeKing/src/com/vjia/jokeking/GetJoke.java
@@ -0,0 +1,23 @@
+package com.vjia.jokeking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.database.Cursor;
+
+public class GetJoke {
+
+	public static List<Joke> lists = new ArrayList<Joke>();
+
+	public static String getNumber(Context context) {
+		// TODO
+		// Cursor cursor=context.getContentResolver().query(uri, projection,
+		// selection, selectionArgs, sortOrder)
+		lists.add(new Joke("This is Joker 1", 100));
+		lists.add(new Joke("This is Joker 2", 10000));
+		lists.add(new Joke("This is Joker 3", 999));
+		return null;
+	}
+
+}
---
+++ JokeKing/src/com/vjia/jokeking/MainActivity.java
@@ -0,0 +1,29 @@
+package com.vjia.jokeking;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	private ListView listView;
+	MyAdapter myAdapter;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		GetJoke.getNumber(this);
+		listView=(ListView) this.findViewById(R.id.listView);
+		myAdapter = new MyAdapter(GetJoke.lists, this);
+		listView.setAdapter(myAdapter);
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
+
+}
