d7cc452d151ea4d0c429fe801342f34d02f7c7fe
Vincent Jia <guofangsky@gmail.com>
2015-03-08 11:04:30 +0800

    update.

--- TranscardQueryer/gen/com/vjia/transcardqueryer/R.java
+++ TranscardQueryer/gen/com/vjia/transcardqueryer/R.java
@@ -28,10 +28,13 @@ public final class R {
         public static final int watermark=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button_query=0x7f080001;
-        public static final int edittext_cardno=0x7f080000;
-        public static final int textview_card_money=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int button_query=0x7f080003;
+        public static final int edittext_cardno=0x7f080002;
+        public static final int linear_layout_body=0x7f080001;
+        public static final int linear_layout_footer=0x7f080005;
+        public static final int linear_layout_header=0x7f080000;
+        public static final int textview_card_money=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
